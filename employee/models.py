374168a815715bf51434737853109cd71b04435c
Ahmedmamdouh40 <ahmedmamdouh2727@gmail.com>
2020-12-27 15:57:47 +0200

    added employee model

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from enum import Enum
 
-# Create your models here.
+class Employee (models.Model):
+    full_name = models.CharField(max_length=150)
+    emp_number = models.IntegerField()
+    
+    GenderChoices=[
+        ('M' , 'Male'),
+        ('F' , 'Female')
+    ]
+    gender = models.CharField(choices=GenderChoices , max_length=1)
+    date_of_birth = models.DateField()
+    nationality = models.CharField(max_length=50)
+    place_of_birth = models.CharField(max_length=150)
+    address = models.CharField(max_length=150)
+    mobile_number = models.CharField(max_length=150)
+    
+    IdTypeChoices=[
+        ('NATIONAL' , 'National'),
+        ('PASSPORT' , 'Passport')
+    ]
+    id_type = models.CharField(choices=IdTypeChoices , max_length=10)
+    id_number = models.CharField(max_length=14)
+    hire_date = models.DateField()
+    
+    SocialStatusChoices=[
+        ('SINGLE' , 'Single'),
+        ('MARRIED' , 'Married')
+    ]
+    social_status = models.CharField(choices=SocialStatusChoices , max_length=10)
+    is_insured = models.BooleanField()
+    is_active = models.BooleanField()
+    has_medical = models.BooleanField()
--- employee_manager/settings.py
+++ employee_manager/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'employee',
+    'contract',
+    'leave',
 ]
 
 MIDDLEWARE = [
