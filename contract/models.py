bc013faa1012711f12054fdd1fcac018cacee5f4
Ahmedmamdouh40 <ahmedmamdouh2727@gmail.com>
2020-12-27 16:27:53 +0200

    added department , position and contract model

--- contract/models.py
+++ contract/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from employee.models import Employee
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=50)
+    department_desc = models.ForeignKey('self' ,on_delete=models.CASCADE ,blank=True, null=True)
+    def __str__(self):
+        return self.name
+
+class Position(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+class Contract(models.Model):
+    emp_name = models.ForeignKey(Employee , on_delete=models.CASCADE)
+    position = models.ForeignKey(Position , on_delete=models.CASCADE)
+    
+    
