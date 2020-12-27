724fbbe44f01e84ff0ab3e20c88bc333dace3249
Ahmedmamdouh40 <ahmedmamdouh2727@gmail.com>
2020-12-27 16:41:37 +0200

    added leaveMaster and employeeLeave model

--- leave/models.py
+++ leave/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from employee.models import Employee
 
-# Create your models here.
+class LeaveMaster(models.Model):
+    name = models.CharField(max_length=20)
+    value = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+class EmployeeLeave(models.Model):
+    emp_name = models.ForeignKey(Employee , on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    resume_date = models.DateField()
+    leave_type = models.ForeignKey(LeaveMaster , on_delete=models.CASCADE)
