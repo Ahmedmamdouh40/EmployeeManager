d93b3a5ae14f89093bf2497900cf045f7693db3b
Ahmedmamdouh40 <ahmedmamdouh2727@gmail.com>
2020-12-29 11:38:36 +0200

    seperated employee_leaves_list in new application {home}

--- employee_manager/views.py
+++ employee_manager/views.py
@@ -1,7 +1,1 @@
 from django.shortcuts import render
-from leave.models import EmployeeLeave
-
-def view_leaves(request):
-    all_leaves = EmployeeLeave.objects.all()
-    context = {'leaves' : all_leaves}
-    return render(request , 'base.html' , context)
